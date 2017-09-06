6421434c9f049dbe5b268179759011defa652f3a
kiran ouseph <kiranouseph@gmail.com>
2017-09-06 22:41:35 +0530

    3rd assignment
    backend

---
+++ ecommerce_backend/src/main/java/com/niit/dao/ProductDAOImpl.java
@@ -0,0 +1,24 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Product;
+
+public class ProductDAOImpl implements ProductDAO{
+
+	public void saveProduct() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Product> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Product getProductById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
